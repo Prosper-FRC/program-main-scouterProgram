cd8b4b61d42ea5e985800949cf80a6dff45a2daa
SteeloIs <sterlingdoesmc@gmail.com>
2023-01-14 13:48:26 -0600

    Controller code

---
+++ Controller.js
@@ -0,0 +1,25 @@
+
+// get user requests and then send it
+// create user data points
+
+function ConeOn()
+{
+    return null; 
+}
+function CubeOn()
+{
+    return null;
+}
+function Cycle()
+{
+    return null;
+}
+
+class GridInput
+{
+    constructor(type, color)
+    {
+        this.type = type;
+        this.color = color;
+    }
+}
