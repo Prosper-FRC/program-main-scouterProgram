36f98b78503ee156c978c156ab8e2e3865edbe64
NumberRaptor482 <49574286+NumberRaptor482@users.noreply.github.com>
2023-02-05 14:49:58 -0600

    Updated admin canvas
    Code is more optimized and readable

--- Rooms/admin.js
+++ Rooms/admin.js
@@ -1,64 +1,59 @@
 const socket = io.connect('http://localhost:5500')
 
-let blueCanvas = document.getElementById("blue-canvas")
-let redCanvas = document.getElementById("red-canvas")
+let canvas = {
+    "blue": document.getElementById("blue-canvas"),
+    "red": document.getElementById("red-canvas")
+}
 
-let blueImage = new Image()
-blueImage.src = "Assets/FRC_PlayingField_blue.png"
-let redImage = new Image()
-redImage.src = "Assets/FRC_PlayingField_red.png"
+let image = {
+    "blue": new Image(),
+    "red": new Image()
+}
+image.blue.src = "Assets/FRC_PlayingField_blue.png"
+image.red.src = "Assets/FRC_PlayingField_red.png"
 
-let blueField = new Field(blueImage, 800, 755)
-blueField.setCanvas(blueCanvas)
-let redField = new Field(redImage, 800, 755)
-redField.setCanvas(redCanvas)
+let field = {
+    "blue": new Field(image.blue, 800, 755),
+    "red": new Field(image.red, 800, 755)
+}
+field.blue.setCanvas(canvas.blue)
+field.red.setCanvas(canvas.red)
 
-let blueGrid = new Grid(blueField.width, blueField.height, 47, 58)
-blueGrid.setCanvas(blueCanvas)
-let redGrid = new Grid(redField.width, redField.height, 47, 58)
-redGrid.setCanvas(redCanvas)
+let grid = {
+    "blue": new Grid(field.blue.width, field.blue.height, 47, 58),
+    "red": new Grid(field.red.width, field.red.height, 47, 58)
+}
+grid.blue.setCanvas(canvas.blue)
+grid.red.setCanvas(canvas.red)
 //let scoreboard = new ScoreBoard()
 
 window.onload = function() {
-    blueCanvas.width = blueField.width
-    blueCanvas.height = blueField.height
-    redCanvas.width = redField.width
-    redCanvas.height = redField.height
+    canvas.blue.width = field.blue.width
+    canvas.blue.height = field.blue.height
 
-    blueField.draw()
-    blueGrid.draw()
-    redField.draw()
-    redGrid.draw()
+    canvas.red.width = field.red.width
+    canvas.red.height = field.red.height
+
+    field.blue.draw()
+    field.red.draw()
+
+    grid.blue.draw()
+    grid.red.draw()
 }
 
 socket.on('connect', () => {
     socket.emit('newAdmin')
 })
 
-socket.on('placeMarker', (color, data) => {
-    if (color == "blue") {
-        blueGrid.placeMarker(data.x, data.y, data.markerColor)
-    } else if (color == "red") {
-        redGrid.placeMarker(data.x, data.y, data.markerColor)
-    }
+socket.on('placeMarker', (color, marker) => {
+    grid[color].placeMarker(marker.x, marker.y, marker.markerColor)
 })
 
-socket.on('redraw', (color, data) => {
-    if (color == "blue") {
-        blueField.clear()
-        blueField.draw()
-        blueGrid.draw()
-        for (let property in data) {
-            let marker = data[property]
-            blueGrid.placeMarker(marker.x, marker.y, marker.markerColor)
-        }
-    } else if (color == "red") {
-        redField.clear()
-        redField.draw()
-        redGrid.draw()
-        for (let property in data) {
-            let marker = data[property]
-            redGrid.placeMarker(marker.x, marker.y, marker.markerColor)
-        }
+socket.on('redraw', (color, markers) => {
+    field[color].clear()
+    field[color].draw()
+    grid[color].draw()
+    for (let marker of markers) {
+        grid[color].placeMarker(marker.x, marker.y, marker.markerColor)
     }
 })
