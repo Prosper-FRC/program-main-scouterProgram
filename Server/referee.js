38796b1aa775c1d860660df8b986074713e985f0
Nikhil Inuganti <ninuganti15@gmail.com>
2024-02-10 11:15:40 -0600

    updated scoring method

--- Server/referee.js
+++ Server/referee.js
@@ -1,52 +1,46 @@
-const gp = require('./gamePieces');
+const gp = require("./gamePieces")
 
-function TileScores(x,y)
-{
-    let score = 0;
-    if(y >= 3)
-    {
-        switch(x)
-        {
-            case 0:
-                score = 5;
-                break;
-            case 1:
-                score = 3;
-                break;
-            case 2:
-                score = 2;
-                break;
-            default:
-                score = 0;
-                break;
-        }
+function TileScores(x, y) {
+  let score = 0
+  if (y >= 3) {
+    switch (x) {
+      case 0:
+        score = 5
+        break
+      case 1:
+        score = 3
+        break
+      case 2:
+        score = 2
+        break
+      default:
+        score = 0
+        break
     }
-    
-    return score;
+  }
+
+  return score
 }
-function TileScoresAlt(x,y)
-{
-    let score = 0;
-    if(y >= 3)
-    {
-        switch(x)
-        {
-            case 11:
-                score = 2;
-                break;
-            case 12:
-                score = 3;
-                break;
-            case 13:
-                score = 5;
-                break;
-            default:
-                score = 0;
-                break;
-        }
+function TileScoresAlt(x, y) {
+  let score = 0
+  if (y >= 3) {
+    switch (x) {
+      case 11:
+        score = 2
+        break
+      case 12:
+        score = 3
+        break
+      case 13:
+        score = 5
+        break
+      default:
+        score = 0
+        break
     }
-    
-    return score;
+  }
+
+  return score
 }
 // function not being used
 // function CoopScores(x,y)
@@ -70,7 +64,7 @@ function TileScoresAlt(x,y)
 //                 break;
 //         }
 //     }
-    
+
 //     return score;
 // }
 // function not being used
@@ -95,87 +89,128 @@ function TileScoresAlt(x,y)
 //                 break;
 //         }
 //     }
-    
+
 //     return score;
 // }
 
-
 class ScoreBoard {
-    constructor() {
-        this.AllianceScore = 0;
-        this.AllianceTeleopScore = 0;
-        this.AllianceAutonScore = 0;
-        this.AllianceAmpScore = 0;
-        this.AllianceSpeakerScore = 0;
-        this.AllianceTrapScore = 0;
-        this.AllianceMobileScore = 0;
-        this.AllianceOnStageScore = 0;
-        this.AllianceSpotlightScore = 0;
-        this.AllianceParkingScore = 0;
-    }
+  constructor() {
+    this.AllianceScore = 0
+    this.AllianceTeleopScore = 0
+    this.AllianceAutonScore = 0
+    this.AllianceAmpScore = 0
+    this.AllianceSpeakerScore = 0
+    this.AllianceTrapScore = 0
+    this.AllianceMobileScore = 0
+    this.AllianceOnStageScore = 0
+    this.AllianceSpotlightScore = 0
+    this.AllianceParkingScore = 0
+    this.AllianceHarmonyScore = 0
+  }
 }
 
-class ScoreLive
-{
-    
-    constructor()
-    {
-        this.sb = new ScoreBoard();   
-    }
-    UpdateMarkers(auton_markers, teleop_markers, team)
-    {
-        // check the score based coords 
-        
-        
-        /** Scoring Instructions * */
+class ScoreLive {
+  constructor() {
+    this.sb = new ScoreBoard()
+  }
+  UpdateMarkers(auton_markers, teleop_markers, team) {
+    // check the score based coords
 
-        // 1. Loop Through Auton Markers
-        // 2. Check the marker type of each marker
-        // 3. Switch
-                // If markerType == 'mobile'
-                        /**Handle Mobile score */
-                // If gameState == 'Auton' && markerType == 'Amplifier'
-                    /** handle amplifier Score*/
-                // If GameState == 'Auton' && markerType == 'Speaker'
-                    /** handle speaker score */
-                // If GameState == 'Teleop' && markerType == 'Speaker'
-                    /** handle Amplifier score */
-                // If GameState == 'Teleop' && markerType == 'Amplifier'
-                    /** handle Amplifier score */
-                // If GameState == 'Amplified' && markerType == 'Speaker'
-                    /** handle amplified speaker score */
-                // If markerType == 'OnStage'
-                    /** handle onstage score */
-                // If markerType == 'Trap'
-                    /** handle trap score */
-                // If markerType == 'Spotlight'
-                    /** handle spotlight score */
-                // If markerType == 'Parked'
-                    /** handle park score */
-      
-        
-        if(team.gameState['teleop'])
-        {
-            team.gameState['teleop'].markerScore = 0;
-            team.gameState['teleop'].parkingScore = 0;
-        }
+    /** Scoring Instructions * */
 
-        if(team.gameState['auton'])
-        {
-            team.gameState['auton'].markerScore = 0;
-            team.gameState['auton'].parkingScore = 0;
-        }
-       
+    /** Scoring Instructions * */
+
+    // 1. Loop Through Auton Markers
+    // 2. Check the marker type of each marker
+    // 3. Switch
+    // If markerType == 'mobile'
+    for (let marker in auton_markers) {
+      if (auton_markers[marker].markerType == "mobile") {
+        this.AllianceAutonScore += 2
+        this.AllianceMobileScore += 2
+      }
+
+      // Handle Auton Scoring
+      if (auton_markers[marker].markerType == "Amplifier") {
+        this.AllianceAmpScore += 2
+        this.AllianceAutonScore += 2
+      }
+
+      if (auton_markers[marker].markerType == "Speaker") {
+        this.AllianceAutonScore += 5
+        this.AllianceSpeakerScore += 5
+      }
     }
 
-    GetRankingPoints(color)
-    {
-        
-    }
-    
-    
-   
+    for (let marker in teleop_markers) {
+      // Handle Teleop Scoring
+      // checks if game state telop and ampState is not amplified
+      if (
+        teleop_markers[marker].markerType == "Speaker" &&
+        ampState != "Amplified"
+      ) {
+        this.AllianceTeleopScore += 2
+        this.AllianceSpeakerScore += 2
+      }
 
+      if (telop_marker[marker].marker.markerType == "Amplifier") {
+        this.AllianceTeleopScore += 1
+        this.AllianceAmpScore += 1
+      }
+
+      // checks if game state telop and ampState is amplified
+      if (
+        telop_marker[marker].marker.markerType == "Speaker" &&
+        ampState == "Amplified"
+      ) {
+        this.AllianceSpeakerScore += 5
+        this.AllianceTeleopScore += 5
+      }
+
+      // Stage Scoring
+      // checks in marker type is onStage and is not Spotlit
+      if (
+        telop_marker[marker].marker.markerType == "OnStage" &&
+        spotlitState != "Spotlit"
+      ) {
+        this.AllianceOnStageScore += 3
+      }
+      // checks if marker type is onStage and is spotlit
+      if (
+        telop_marker[marker].marker.markerType == "OnStage" &&
+        spotlitState == "Spotlit"
+      ) {
+        this.AllianceOnStageScore += 4
+      }
+      // limits trap scoring to 15 for a max of 3 trap scores per alliance
+      if (
+        telop_marker[marker].marker.markerType == "Trap" &&
+        this.AllianceTrapScore < 15
+      ) {
+        this.AllianceTrapScore += 5
+      }
+
+      if (telop_marker[marker].marker.markerType == "Parked") {
+        this.AllianceParkingScore += 1
+      }
+
+      if (telop_marker[marker].marker.markerType == "Harmony") {
+        this.AllianceHarmonyScore += 2
+      }
+    }
+
+    if (team.gameState["teleop"]) {
+      team.gameState["teleop"].markerScore = 0
+      team.gameState["teleop"].parkingScore = 0
+    }
+
+    if (team.gameState["auton"]) {
+      team.gameState["auton"].markerScore = 0
+      team.gameState["auton"].parkingScore = 0
+    }
+  }
+
+  GetRankingPoints(color) {}
 
   /*  GetRankingPoints(team)
     {
@@ -215,65 +250,47 @@ class ScoreLive
 
         return rPoints;
     }*/
-    
-    // links is working :D
-    // dispite the pain
 
-   
+  // links is working :D
+  // dispite the pain
 
-    Penalty(PenType, team)
-    {
-        amount = 0;
-        switch(PenType)
-        {
-            case "foul":
-                amount = 5;
-                break;
-            case "tech foul": 
-                amount = 12;
-                break
-            case "red card": 
-                amount = null;
-                break;
-        }
-        if(team == "blue")
-        {
-            if(amount == null)
-            {
-                this.sb.blueAllianceScore = 0;
-            }
-            else 
-            {
-                this.sb.redAllianceScore += amount;
-            }
-        }
-        if(team == "red")
-        {
-            if(amount == null)
-            {
-                this.sb.redAllianceScore = 0;
-            }
-            else 
-            {
-                this.sb.blueAllianceScore += amount;
-            }
-        }
+  Penalty(PenType, team) {
+    amount = 0
+    switch (PenType) {
+      case "foul":
+        amount = 5
+        break
+      case "tech foul":
+        amount = 12
+        break
+      case "red card":
+        amount = null
+        break
     }
-    TeamScore(team)
-    {
-        if(team == "blue")
-            return this.sb.AllianceScore;
-        if(team == "red")
-            return this.sb.AllianceScore;
-        return 0;
+    if (team == "blue") {
+      if (amount == null) {
+        this.sb.blueAllianceScore = 0
+      } else {
+        this.sb.redAllianceScore += amount
+      }
     }
-    
-    GetBoard()
-    {
-        return this.sb;
+    if (team == "red") {
+      if (amount == null) {
+        this.sb.redAllianceScore = 0
+      } else {
+        this.sb.blueAllianceScore += amount
+      }
     }
+  }
+  TeamScore(team) {
+    if (team == "blue") return this.sb.AllianceScore
+    if (team == "red") return this.sb.AllianceScore
+    return 0
+  }
 
-
+  GetBoard() {
+    return this.sb
+  }
 }
 
-module.exports = {TileScores,ScoreLive}
+module.exports = { TileScores, ScoreLive }
